a1b4b0136959c84daaae1a7fde5bb854bd958351
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-02 23:25:45 +0900

    V4 Flyway

---
+++ gout-backend/src/main/resources/db/migration/V4__create_booking_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "booking" (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES "user"(id),
+    tour_id INTEGER NOT NULL REFERENCES "tour"(id),
+    state VARCHAR(20) NOT NULL,
+    booking_date TIMESTAMP NOT NULL,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
