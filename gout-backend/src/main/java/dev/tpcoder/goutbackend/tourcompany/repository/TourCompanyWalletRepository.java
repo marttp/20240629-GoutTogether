a865549f9d89f0d89d740ed5684b2ea8be0287ef
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-04 14:44:32 +0900

    Add TourCompanyWalletRepository

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/tourcompany/repository/TourCompanyWalletRepository.java
@@ -0,0 +1,9 @@
+package dev.tpcoder.goutbackend.tourcompany.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import dev.tpcoder.goutbackend.tourcompany.model.TourCompanyWallet;
+
+public interface TourCompanyWalletRepository extends CrudRepository<TourCompanyWallet, Integer> {
+
+}
