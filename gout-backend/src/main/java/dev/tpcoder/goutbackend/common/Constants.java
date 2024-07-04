8169ca14ce43429215ea9e627dc5e0e7de61fd12
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-04 14:39:45 +0900

    Add constant value for role

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/common/Constants.java
@@ -0,0 +1,10 @@
+package dev.tpcoder.goutbackend.common;
+
+public class Constants {
+    
+    private Constants() {}
+
+    public static final String ROLE_CONSUMER = "ROLE_CONSUMER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_COMPANY = "ROLE_COMPANY";
+}
