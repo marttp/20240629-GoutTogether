217e7a55bf65191c7fac5329401addd9e06b7134
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-04 14:45:24 +0900

    Add TourCompanyWallet table

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/tourcompany/model/TourCompanyWallet.java
@@ -0,0 +1,17 @@
+package dev.tpcoder.goutbackend.tourcompany.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("tour_company_wallet")
+public record TourCompanyWallet(
+        @Id Integer id,
+        AggregateReference<TourCompany, Integer> tourCompanyId,
+        Instant lastUpdated,
+        BigDecimal balance) {
+
+}
