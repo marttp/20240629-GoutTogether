e698919de9042692bc9988afbae5aa95a50373ad
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-04 12:15:34 +0900

    temporary for recording video

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/tourcompany/TourCompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.tpcoder.goutbackend.tourcompany;
-
-import org.springframework.data.repository.CrudRepository;
-
-import dev.tpcoder.goutbackend.tourcompany.model.TourCompany;
-
-public interface TourCompanyRepository extends CrudRepository<TourCompany, Integer> {
-
-}
