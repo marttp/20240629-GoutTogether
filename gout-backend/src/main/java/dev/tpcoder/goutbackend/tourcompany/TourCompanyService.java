2ed2072f557b1b71674a7446c2f22d8dfb8ca3c3
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-04 12:15:37 +0900

    temporary for recording video

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/tourcompany/TourCompanyService.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/tourcompany/TourCompanyService.java
@@ -1,11 +1,13 @@
 package dev.tpcoder.goutbackend.tourcompany;
 
 import dev.tpcoder.goutbackend.tourcompany.model.TourCompany;
-import dev.tpcoder.goutbackend.tourcompany.model.TourCompanyDto;
+import dev.tpcoder.goutbackend.tourcompany.model.RegisterTourCompanyDto;
 
 public interface TourCompanyService {
 
-    TourCompany registerTourCompany(TourCompanyDto payload);
+    TourCompany registerTourCompany(RegisterTourCompanyDto payload);
 
     TourCompany approvedTourCompany(Integer id);
+
+    void createLoginContext(RegisterTourCompanyDto body, TourCompany tourCompany);
 }
