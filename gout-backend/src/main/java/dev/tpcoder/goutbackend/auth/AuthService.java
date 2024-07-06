2f9df36e7c808bcab17d310597daf1787dd27bfe
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-06 15:32:46 +0900

    Add AuthService

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/AuthService.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/AuthService.java
@@ -1,5 +1,14 @@
 package dev.tpcoder.goutbackend.auth;
 
+import java.util.Optional;
+
 public interface AuthService {
 
+    Optional<UserLogin> findCredentialByUserId(int userId);
+
+    Optional<UserLogin> findCredentialByUsername(String email);
+
+    UserLogin createConsumerCredential(int userId, String email, String password);
+
+    void deleteCredentialByUserId(int userId);
 }
