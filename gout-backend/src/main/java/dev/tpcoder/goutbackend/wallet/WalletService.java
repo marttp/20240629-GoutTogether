9b5a9f1e50fae8fb09222c131df7f6868437e93c
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-06 15:34:02 +0900

    Add WalletService

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/wallet/WalletService.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/wallet/WalletService.java
@@ -1,5 +1,10 @@
 package dev.tpcoder.goutbackend.wallet;
 
+import dev.tpcoder.goutbackend.wallet.model.UserWallet;
+
 public interface WalletService {
 
+    UserWallet createConsumerWallet(int userId);
+
+    void deleteConsumerWalletByUserId(int userId);
 }
