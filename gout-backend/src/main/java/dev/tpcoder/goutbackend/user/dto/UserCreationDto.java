dee5f98f4a080cd58b17a049c0f4eb70cd53f685
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-06 12:56:06 +0900

    Add UserCreationDto

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/user/dto/UserCreationDto.java
@@ -0,0 +1,12 @@
+package dev.tpcoder.goutbackend.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UserCreationDto(
+        @NotBlank String firstName,
+        @NotBlank String lastName,
+        String phoneNumber,
+        @NotBlank String email,
+        @NotBlank String pasword) {
+
+}
