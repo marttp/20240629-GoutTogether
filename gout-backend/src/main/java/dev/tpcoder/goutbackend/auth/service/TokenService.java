54f6b480a5ee3cdbe805a59ff12b6930397a7198
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-07 19:43:16 +0900

    TokenService for generate JWT

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/service/TokenService.java
@@ -0,0 +1,38 @@
+package dev.tpcoder.goutbackend.auth.service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder jwtEncoder;
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication auth) {
+        Instant now = Instant.now();
+
+        String scope = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("gout-backend")
+                .issuedAt(now)
+                .subject(auth.getName())
+                .claim("roles", scope)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
