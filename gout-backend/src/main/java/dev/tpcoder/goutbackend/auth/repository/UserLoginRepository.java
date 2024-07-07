77245fba1a00aa4063a22831aa7651b63394e5fc
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-07 19:42:22 +0900

    Move directory

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/repository/UserLoginRepository.java
@@ -0,0 +1,15 @@
+package dev.tpcoder.goutbackend.auth.repository;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+
+import dev.tpcoder.goutbackend.auth.model.UserLogin;
+import dev.tpcoder.goutbackend.user.model.User;
+
+public interface UserLoginRepository extends CrudRepository<UserLogin, Integer> {
+
+    Optional<UserLogin> findOneByEmail(String email);
+
+    Optional<UserLogin> findOneByUserId(AggregateReference<User,Integer> userId);
+}
