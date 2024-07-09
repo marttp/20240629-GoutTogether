392d56a852529a7fc82ecbb923e813dbf99368a1
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-09 23:26:52 +0900

    Enhance RefreshTokenRepository to able to query by findOneByToken

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/repository/RefreshTokenRepository.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/repository/RefreshTokenRepository.java
@@ -1,5 +1,7 @@
 package dev.tpcoder.goutbackend.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,4 +15,6 @@ public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Int
     @Query("UPDATE refresh_token SET is_expired = :is_expired WHERE usage = :usage AND resource_id = :resource_id")
     void updateRefreshTokenByResource(@Param("usage") String usage, @Param("resource_id") int resourceId,
             @Param("is_expired") boolean isExpired);
+
+    Optional<RefreshToken> findOneByToken(String token);
 }
