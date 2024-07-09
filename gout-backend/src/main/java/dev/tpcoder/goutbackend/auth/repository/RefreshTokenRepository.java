b6e6c2ad5ac447905e31b6cd4dde5fba8dafc7ce
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-09 11:39:29 +0900

    Add RefreshTokenRepository with Query method

---
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package dev.tpcoder.goutbackend.auth.repository;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import dev.tpcoder.goutbackend.auth.model.RefreshToken;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+
+    @Modifying
+    @Query("UPDATE refresh_token SET is_expired = :is_expired WHERE usage = :usage AND resource_id = :resource_id")
+    void updateRefreshTokenByResource(@Param("usage") String usage, @Param("resource_id") int resourceId,
+            @Param("is_expired") boolean isExpired);
+}
