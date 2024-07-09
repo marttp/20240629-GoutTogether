95f54b36ef3bbcfc477301c20088b6245920a122
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-09 23:27:30 +0900

    Add refreshToken controller

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/controller/AuthController.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/auth/controller/AuthController.java
@@ -1,19 +1,19 @@
 package dev.tpcoder.goutbackend.auth.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import dev.tpcoder.goutbackend.auth.dto.LoginRequestDto;
-import dev.tpcoder.goutbackend.auth.dto.LoginResponseDto;
-import dev.tpcoder.goutbackend.auth.dto.LogoutDto;
-import dev.tpcoder.goutbackend.auth.service.AuthService;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.tpcoder.goutbackend.auth.dto.LoginRequestDto;
+import dev.tpcoder.goutbackend.auth.dto.LoginResponseDto;
+import dev.tpcoder.goutbackend.auth.dto.LogoutDto;
+import dev.tpcoder.goutbackend.auth.dto.RefreshTokenDto;
+import dev.tpcoder.goutbackend.auth.service.AuthService;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -28,6 +28,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody @Validated LoginRequestDto body) {
         return ResponseEntity.ok(authService.login(body));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refreshToken(@RequestBody @Validated RefreshTokenDto body) {
+        return ResponseEntity.ok(authService.issueNewAccessToken(body));
     }
 
     @PostMapping("/logout")
