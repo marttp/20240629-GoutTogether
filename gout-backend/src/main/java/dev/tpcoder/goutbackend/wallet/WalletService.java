d5e8eeb1a0b2d4fd4cb336ccc83c96a8c099c13d
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-07-13 20:39:47 +0900

    Enhance WalletService - getOwnWallet & topup

--- gout-backend/src/main/java/dev/tpcoder/goutbackend/wallet/WalletService.java
+++ gout-backend/src/main/java/dev/tpcoder/goutbackend/wallet/WalletService.java
@@ -1,5 +1,7 @@
 package dev.tpcoder.goutbackend.wallet;
 
+import dev.tpcoder.goutbackend.wallet.dto.TopupDto;
+import dev.tpcoder.goutbackend.wallet.dto.UserWalletInfoDto;
 import dev.tpcoder.goutbackend.wallet.model.UserWallet;
 
 public interface WalletService {
@@ -7,4 +9,8 @@ public interface WalletService {
     UserWallet createConsumerWallet(int userId);
 
     void deleteConsumerWalletByUserId(int userId);
+
+    UserWalletInfoDto getOwnWallet(int userId);
+
+    UserWalletInfoDto topup(TopupDto body);
 }
